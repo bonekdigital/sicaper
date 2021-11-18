6728c89b8fd2ca60b2929393c7394bb17d3ff131
bonekdigital <94593724+bonekdigital@users.noreply.github.com>
2021-11-18 13:53:25 +0700

    Update and rename h2h-persebaya-borneofc.html to h2h-persebaya-borneofc.js

---
+++ h2h-persebaya-borneofc.js
@@ -0,0 +1,55 @@
+
+<div class='table'>
+  <table style='white-space: nowrap;'>
+<thead>
+  <tr>
+    <th>Musim</th>
+    <th>Kompetisi</th>
+    <th colspan="3">Head-to-Head</th>
+    <th></th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>2018</td>
+    <td>Liga 1</td>
+    <td>Borneo FC</td>
+    <td>2-2</td>
+    <td>Persebaya</td>
+    <td><a href="https://bonekdigital.my.id/" target="_blank" rel="noopener noreferrer">Report</a></td>
+  </tr>
+  <tr>
+    <td>2018</td>
+    <td>Liga 1</td>
+    <td>Persebaya</td>
+    <td>0-1</td>
+    <td>Borneo FC</td>
+    <td><a href="https://bonekdigital.my.id/" target="_blank" rel="noopener noreferrer">Report</a></td>
+  </tr>
+  <tr>
+    <td>2019</td>
+    <td>Liga 1</td>
+    <td>Borneo FC</td>
+    <td>1-2</td>
+    <td>Persebaya</td>
+    <td><a href="https://bonekdigital.my.id/" target="_blank" rel="noopener noreferrer">Report</a></td>
+  </tr>
+  <tr>
+    <td>2019</td>
+    <td>Liga 1</td>
+    <td>Persebaya</td>
+    <td>0-0</td>
+    <td>Borneo FC</td>
+    <td><a href="https://bonekdigital.my.id/" target="_blank" rel="noopener noreferrer">Report</a></td>
+  </tr>
+  <tr>
+    <td>2021</td>
+    <td>Liga 1</td>
+    <td>Borneo FC</td>
+    <td>3-1</td>
+    <td>Persebaya</td>
+    <td><a href="https://bonekdigital.my.id/" target="_blank" rel="noopener noreferrer">Report</a></td>
+  </tr>
+</tbody>
+</table>
+    </div>
